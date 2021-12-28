05dddc2b7d1e2314596029bd079e6682aee94322
GabrielPe24 <58280841+GabrielPe24@users.noreply.github.com>
2021-12-28 12:16:14 -0300

    Updated

---
+++ Autonumeracion/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Autonumeracion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton11 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage_1);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Inicio:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(91, 94);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(91, 41);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Fin:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(237, 201);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Limites de Numeración";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(91, 151);
+            this.numericUpDown3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Intervalos:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton11);
+            this.groupBox2.Controls.Add(this.radioButton10);
+            this.groupBox2.Controls.Add(this.radioButton9);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(256, 14);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(507, 649);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Vista Preliminar";
+            // 
+            // radioButton11
+            // 
+            this.radioButton11.AutoSize = true;
+            this.radioButton11.Location = new System.Drawing.Point(280, 39);
+            this.radioButton11.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton11.Name = "radioButton11";
+            this.radioButton11.Size = new System.Drawing.Size(136, 21);
+            this.radioButton11.TabIndex = 14;
+            this.radioButton11.TabStop = true;
+            this.radioButton11.Text = "Formulario N° 11";
+            this.radioButton11.UseVisualStyleBackColor = true;
+            this.radioButton11.CheckedChanged += new System.EventHandler(this.radioButton11_CheckedChanged);
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(8, 40);
+            this.radioButton10.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(128, 21);
+            this.radioButton10.TabIndex = 15;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "Formulario N° 2";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            this.radioButton10.CheckedChanged += new System.EventHandler(this.radioButton10_CheckedChanged);
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(144, 39);
+            this.radioButton9.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(128, 21);
+            this.radioButton9.TabIndex = 16;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "Formulario N° 3";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            this.radioButton9.CheckedChanged += new System.EventHandler(this.radioButton9_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(12, 217);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 65);
+            this.button1.TabIndex = 17;
+            this.button1.Tag = "s";
+            this.button1.Text = "Imprimir";
+            this.button1.UseCompatibleTextRendering = true;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = global::Autonumeracion.Properties.Resources.Borde1;
+            this.pictureBox1.Image = global::Autonumeracion.Properties.Resources.A4___1;
+            this.pictureBox1.InitialImage = global::Autonumeracion.Properties.Resources.Borde;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 67);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(493, 578);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 674);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(783, 721);
+            this.MinimumSize = new System.Drawing.Size(783, 721);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Autonumeración";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton11;
+        private System.Windows.Forms.RadioButton radioButton10;
+    }
+}
+
---
+++ Autonumeracion/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Autonumeracion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private long num = 0; //Declaro una variable privada num, la cual se usara para la cantidad de paginas finales.
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            long pagini = Convert.ToInt64(numericUpDown2.Text);     //Declaro e igualo la variable pagina inicial.
+            long pagfnl = Convert.ToInt64(numericUpDown1.Text);     //Declaro e igualo la variable pagina final.
+            long intervalos = Convert.ToInt64(numericUpDown3.Text); //Declaro e igualo la variable intervalos.
+
+            if (radioButton9.Checked == false && radioButton10.Checked == false && radioButton11.Checked == false) //Verifico que se seleccione un tipo de formulario.
+            {
+                MessageBox.Show("Debes seleccionar un tipo de formulario, para poder imprimir.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
+
+            else if ((num + pagfnl) == 0) //Verifico que la pagina inicial no se 0.
+            {
+                MessageBox.Show("La pagina inicial no puede ser CERO.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
+
+            else if ((num + pagfnl) > pagini) //Verifico que la pagina final no se menor a la pagina inicial.
+            {
+                MessageBox.Show("La pagina inicial no puede ser mayor a la pagina final.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
+
+            else if (intervalos == 0) //Verifico que intervalos no sea 0.
+            {
+                MessageBox.Show("El intervalo no puede ser CERO.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
+
+            else if (radioButton10.Checked == true) //Si el Formulario es el N° 11. Uso Hoja Tamaño Legal.
+            {
+                printDocument1 = new PrintDocument();
+                PrinterSettings ps = new PrinterSettings();
+                printDocument1.PrinterSettings = ps;
+                printDocument1.PrintPage += printDocument1_PrintPage;
+                PaperSize legal = new PaperSize();
+                legal.RawKind = 5;
+                printDocument1.DefaultPageSettings.PaperSize = legal;    
+                printDocument1.Print();
+            }
+            else
+            {
+                printDocument1 = new PrintDocument();
+                PrinterSettings ps = new PrinterSettings();
+                printDocument1.PrinterSettings = ps;
+                printDocument1.PrintPage += printDocument1_PrintPage;
+                printDocument1.Print();
+            }
+        }
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            try
+            {
+                Graphics graphic = e.Graphics;
+                SolidBrush brush = new SolidBrush(Color.Black);
+
+                Font font = new Font("Times New Roman", 12, FontStyle.Bold);
+                Font fon2 = new Font("Courier New", 22, FontStyle.Bold);
+
+                float pageWidth = e.PageSettings.PrintableArea.Width;
+                float pageHeight = e.PageSettings.PrintableArea.Height;
+
+                float fontHeight = font.GetHeight();
+
+                int startXI = 63;     //Posicion Izquierda.
+                int startXC = 180;    //Posicion Centro.
+                int startXD = 700;    //Posicion Derecha.
+
+                int startY  = 95;     //Valor inicial de Y.
+                int startY2 = 35;     //Valor inicial de Y2.
+                int startY3 = 190;    //Valor inicial de Y3.
+
+                int offsetY2 = 10;    //Posicion Superior Vertical y Orizontal.
+
+                long pagini = Convert.ToInt64(numericUpDown2.Text);
+                long pagfnl = Convert.ToInt64(numericUpDown1.Text);
+                long intervalos = Convert.ToInt64(numericUpDown3.Text);
+                int contador = 0;                
+
+                if (radioButton11.Checked == true) //Imprimo Formulario N° 11.
+                {
+                    int Numero = 0;    
+                    while ((num + pagfnl) <= pagini)
+                    {
+                        
+                        var Valor1 = (num + pagfnl);
+                        var Valor2 = (Valor1 + 1);   
+
+                        var Pagina1 = Valor1.ToString("D6"); //Relleno con ceros a la izquierda.
+                        var Pagina2 = Valor2.ToString("D6"); //Relleno con ceros a la izquierda.
+
+                        graphic.DrawString(Pagina1, fon2, brush, startXI, startY3 + offsetY2);        
+                        graphic.DrawString(Pagina1, fon2, brush, (startXD - 455), startY3 + offsetY2);
+                        graphic.DrawString(Pagina2, fon2, brush, (startXI + 555), startY3 + offsetY2);
+                        graphic.DrawString(Pagina2, fon2, brush, (startXD - 255), startY3 + offsetY2);
+                        
+                        Numero = Numero + 1;
+
+                        if (((Numero % 5) == 0) || ((Numero % 5) == 5))
+                        {
+                            offsetY2 += ((int)fontHeight + 300); //Espacios entre ambos numeros.
+                        }
+                        else
+                        {
+                            offsetY2 += ((int)fontHeight + 170); //Espacios entre ambos numeros.
+                            num = num + 1;
+                        }
+
+                        if (offsetY2 >= pageHeight)
+                        {
+                            e.HasMorePages = true;
+                            offsetY2 = 0;
+                            return;
+                        }
+                        else
+                        {
+                            e.HasMorePages = false;
+                        }
+                        num = num + intervalos;
+                    }
+                    pagini = 0;
+                    pagfnl = 0;
+                    intervalos = 0;
+                    contador = 0;
+                    num = 0;
+                }
+
+                else if (radioButton10.Checked == true) //Imprimo Formulario N° 2.
+                {
+                    while ((num + pagfnl) <= pagini)
+                    {
+                        contador = contador + 1;
+
+                        RectangleF rectF1 = new RectangleF(157, 30, 120, 45);  //Arriba-Izquierda.
+                        RectangleF rectF2 = new RectangleF(667, 30, 120, 45);  //Arriba-Derecha.
+                        RectangleF rectF3 = new RectangleF(157, 655, 120, 45); //Abajo-Izquierda.
+                        RectangleF rectF4 = new RectangleF(667, 655, 120, 45); //Abajo-Derecha.
+                        
+                        Image newImage = Autonumeracion.Properties.Resources.Borde1;
+
+                        e.Graphics.DrawImage(newImage, rectF1); //Imagen-Arriba-Izquierda.
+                        e.Graphics.DrawImage(newImage, rectF2); //Imagen-Arriba-Derecha.
+
+                        graphic.DrawString("N° " + (num + pagfnl), font, brush, startXC, startY2 + offsetY2);
+                        graphic.DrawString("N° " + (num + pagfnl), font, brush, startXD, startY2 + offsetY2);
+                        
+                        if ((contador % 2) == 0)
+                        {
+                            offsetY2 += ((int)fontHeight + 805);    //Espacios entre ambos numeros. 
+                            e.Graphics.DrawImage(newImage, rectF3); //Imagen-Abajo-Izquierda.
+                            e.Graphics.DrawImage(newImage, rectF4); //Imagen-Abajo-Derecha.
+                        }
+                        else
+                        {
+                            offsetY2 += ((int)fontHeight + 605); //Espacios entre ambos numeros.
+                        }
+
+                        if (offsetY2 >= (pageHeight + 605))
+                        {
+                            e.HasMorePages = true;
+                            offsetY2 = 0;
+                            return;
+                        }
+                        else
+                        {
+                            e.HasMorePages = false;
+                        }
+                        num = num + intervalos;
+                    }
+                    pagini = 0;
+                    pagfnl = 0;
+                    intervalos = 0;
+                    contador = 0;
+                    num = 0;
+                }
+
+                else if (radioButton9.Checked == true) //Imprimo Formulario N° 3.
+                {
+                    while ((num + pagfnl) <= pagini)
+                    {
+                        contador = contador + 1;
+                        graphic.DrawString("N° " + (num + pagfnl), font, brush, startXD, startY + offsetY2);
+                        
+                        if ((contador % 3) == 0)
+                        {
+                            offsetY2 += ((int)fontHeight + 490); //Espacios entre ambos numeros.
+                        }
+                        else
+                        {
+                            offsetY2 += ((int)fontHeight + 420); //Espacios entre ambos numeros.    
+                        }
+
+                        if (offsetY2 >= (pageHeight + 420))
+                        {
+                            e.HasMorePages = true;
+                            offsetY2 = 0;
+                            return;
+                        }
+                        else
+                        {
+                            e.HasMorePages = false;
+                            
+                        }
+                        num = num + intervalos;
+                    }
+                    pagini = 0;
+                    pagfnl = 0;
+                    intervalos = 0;
+                    contador = 0;
+                    num = 0;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Autonumeracion.Properties.Resources.A4___1;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Autonumeracion.Properties.Resources.A4___2;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Autonumeracion.Properties.Resources.A4___3;
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Autonumeracion.Properties.Resources.A4___4;
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Autonumeracion.Properties.Resources.A4___5;
+        }
+
+        private void radioButton8_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Autonumeracion.Properties.Resources.A4___6;
+        }
+
+        private void radioButton9_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Autonumeracion.Properties.Resources.Hoja3;
+        }
+
+        private void radioButton11_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Autonumeracion.Properties.Resources.Hoja11;
+        }
+
+        private void radioButton10_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Autonumeracion.Properties.Resources.Hoja2;
+        }
+
+        private void printDocument1_PrintPage_1(object sender, PrintPageEventArgs e)
+        {
+
+        }
+
+    }
+}
